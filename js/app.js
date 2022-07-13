c0f6dc86c0bd0e43204925f0b1122ecb91aa4112
alfille <paul.alfille@gmail.com>
2022-07-12 20:57:27 -0400

    Working version with full graphics

--- js/app.js
+++ js/app.js
@@ -85,6 +85,7 @@ class Game {
             e.forEach( ee => current_fox[h] ||= old_locations[ee] );
             e.forEach( ee => current_stats[h] += old_stats[ee]/e.length );
         }
+
         // store
         this.fox_history[this.date] = current_fox;
         this.stats_history[this.date] = current_stats;
@@ -99,7 +100,7 @@ class Game {
     }
 
     get prior() {
-        return [this.inspections[this.date-1],this.fox_history[this.date-1]];
+        return [this.inspections[this.date-1]||[],[],this.fox_history[this.date-1]];
     }
 
     back() { // backup a move
@@ -116,11 +117,93 @@ class Game {
 }
 var G = new Game();
 
+class GardenView {
+    constructor() {
+        this.svg = document.getElementById("svg");
+        this.head = document.getElementById("top");
+        window.onresize = this.dimension_control();
+    }
+
+    start() {
+        this.history = [] ;
+    }
+
+    back() {
+        this.history.pop();
+    }
+
+    add_history_row(s) {
+        let Th = s.map( (_,i) => `<g id=${"bottom_"+i}><use href="#old_hole" transform="rotate(${360.*i/s.length})" /></g>`).join("");
+        let Tf = s.map( (ss,i) => `<g transform="rotate(${360.*i/s.length})"><text class="old_fox" x="42" y="580" rotate="180">${ss}</text></g>`).join("");
+        this.history.push( `<circle cx="0" cy="0" r="600" stroke="grey" stroke-width="3" fill="none" />
+            <def><circle id="old_hole" cx="0" cy="600" r="50" /></def>
+            ${Th}${Tf}`);
+    }
+        
+    show_history() {
+        if ( this.history.length == 0 ) {
+            return "";
+        }
+        return this.history.reduce( (t,x) => `<g transform="scale(.86) rotate(5)">${t}</g>${x}` );
+    }
+
+    create_svg(s) {
+        let f = G.foxes ;
+        let Th = s.map( (_,i) => `<circle class="svg_hole" cx="0" cy="800" r="150" transform="rotate(${360.*i/s.length})"/>`).join("");
+        let Tf = s.map( (ss,i) => `<g transform="rotate(${360.*i/s.length})"><text class="svg_fox" x="125" y="740" rotate="180">${ss}</text></g>`).join("");
+        let Tl = f.map( (ff,i) => `<use href=${ff?"#svg_larrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join("");
+        let Tr = f.map( (ff,i) => `<use href=${ff?"#svg_rarrow":"#svg_nofox"} transform="rotate(${360.*i/f.length})" />`).join("");
+        let Tc = s.map( (_,i) => `<circle class="svg_click" cx="0" cy="800" r="150" id=${"top_"+i} transform="rotate(${360.*i/s.length})" onmouseover="this.style.stroke='red'" onmouseout="this.style.stroke='black'"/>`).join("");
+        return `<svg viewBox="-1000 -1000 2000 2000"> preserveAspectRatio="xMidYMid meet" width="100%"
+            <circle cx="0" cy="0" r="803" stroke="grey" stroke-width="3" fill="none" />
+            <circle cx="0" cy="0" r="797" stroke="grey" stroke-width="3" fill="none" />
+            <def>
+                <text id="svg_rarrow" x="100" y="790" rotate="-15">&#8594;</text>
+                <text id="svg_larrow" x="-300" y="740" rotate="15">&#8592;</text>
+                <circle id="svg_click" cx="0" cy="800" r="150" />
+            </def>
+            ${Th}
+            ${Tf}
+            ${Tl}
+            ${Tr}
+            ${Tc}
+            ${this.show_history()}
+            <rect id="bottom_back" />
+            <text id="text_back" x="720" y="-870">Back</text />
+            <rect id="top_back"  onmouseover="this.style.stroke='red'" onmouseout="this.style.stroke='black'"/>
+            Sorry, your browser does not support inline SVG.  
+        </svg>` ;
+    }
+
+    dimension_control() {
+        let x = Math.min(this.svg.clientWidth,window.innerHeight-this.head.offsetHeight);
+        this.svg.style.width = x+"px";
+        this.svg.style.height = x+"px";
+    }
+
+
+    control_row(symbol_list) {
+        this.svg.innerHTML = this.create_svg(symbol_list) ;
+        if ( G.number !== 0 ) {
+            this.svg.onload = G.foxes.forEach( (_,i)=>document.getElementById("top_"+i).addEventListener('click', (e) => this.click(e.target)) );
+        }
+        this.svg.onload = document.getElementById("top_back").addEventListener('click', (e) => T.back() );
+        this.svg.onload = this.dimension_control() ;
+    }
+
+    click(target) {
+        let hole = parseInt(target.id.split('_')[1]) ;
+        T.click(hole);
+        target.style.strokeWidth = T.checked(hole) ? "30" : "10" ;
+    }
+}
+var GV = new GardenView();
+
 class Table {
     constructor() {
         this.table = document.querySelector("table") ;
-        this.thead = document.querySelector("thead") ;
-        this.tbody = document.querySelector("tbody") ;
+        this.thead = this.table.querySelector("thead") ;
+        this.tbody = this.table.querySelector("tbody") ;
         this.stats = false;
         this.start() ;
     }
@@ -152,24 +235,37 @@ class Table {
     start() {
         this.header();
         G.start();
+        GV.start();
         this.tbody.innerHTML = "";
         this.control_row();
         this.update();
     }
     
-    check(hole) {
-        let h = [...this.tbody.lastElementChild
+    click(hole) {
+        [...this.tbody.lastElementChild
             .querySelectorAll("input")]
+            .filter( i=>parseInt(i.getAttribute("data-n"))==hole )[0].click();
+    }
+
+    check(hole) {
+        let h = [...this.tbody.lastElementChild.querySelectorAll("input")]
             .filter( c=>c.checked )
             .map(c=>parseInt(c.getAttribute("data-n")));
         if ( h.length == 2 ) {
-            T.move(h) ;
+            this.move(h) ;
         }
+    }
+
+    checked(hole) {
+        return [...this.tbody.lastElementChild.querySelectorAll("input")]
+            .filter( c=>parseInt(c.getAttribute("data-n"))==hole )[0]
+            .checked ;
     }
 
     control_row() {
         let f = G.foxes ;
         let r = document.createElement("tr");
+        let s = this.symbols( [], [], f );
         for ( let i = 0; i <= H.value ; ++i ) {
             let d = document.createElement("td");
             if ( i==0 ) {
@@ -177,8 +273,10 @@ class Table {
                 b.innerText = "Back" ;
                 b.onclick = () => T.back() ;
                 d.appendChild(b);
+            } else if ( G.number == 0 ) {
+                d.innerHTML = s[i-1];
             } else {
-                d.innerHTML = (f[i-1] ? "&#129418" : "&nbsp;") + "<br>" ;
+                d.innerHTML = s[i-1] + "<br>" ;
                 let b = document.createElement("input");
                 b.type = "checkbox";
                 b.onclick = () => T.check(i-1) ;
@@ -189,6 +287,7 @@ class Table {
         }
         let d = document.createElement("td");
         this.tbody.appendChild(r);
+        GV.control_row(s);
     }
 
     back() {
@@ -198,6 +297,7 @@ class Table {
             this.remove_row();
             this.remove_row();
             G.back();
+            GV.back();
             this.control_row();
         }
         this.update();
@@ -215,41 +315,47 @@ class Table {
     move(holes) { // hole 0-based
         G.move(holes);
         this.remove_row();
-        this.static_row();
-        if ( G.number == 0 ) {
-            this.victory_row();
-        } else {
-            this.control_row();
-        }
+        this.add_history_row();
+        this.control_row();
         this.update();
     }
 
-    static_row() {
-        let [m,f] = G.prior ;
+    symbols( moves, poisons, foxes ) {
+        // moves = list of inspection holes
+        // poisons = list of poisoned holes
+        // foxes = true/false fox occupation list
+        // returns a symbol list
+        if ( G.number == 0 && moves.length==0 ) {
+            // victory
+            return foxes.map( (_,i) => i&1 ? "&#128077;" : "&#128516;" ) ;
+        } else {
+            let s = foxes.map( f => f?"&#129418;":"&nbsp;" ) ;
+            moves.forEach( m => s[m] = "&#x274c;" );
+            poisons.forEach( p => s[p] = "&#9760;" );
+            return s ;
+        }
+    } 
+
+    add_history_row() { // historical row
+        let [m,p,f] = G.prior ;
         let r = document.createElement("tr");
+        let s = this.symbols( m, p, f );
         for ( let i = 0; i <= H.value ; ++i ) {
-            let h = i-1; // actual hole index after first column
             let d = document.createElement("td");
             if ( i==0 ) {
                 d.innerHTML = `Day ${G.day-1}`;
-            } else if ( m.indexOf(h) > -1 ) {
-                d.innerHTML = "&#x274c" ;
-            } else if (f[h]) {
-                d.innerHTML = "&#129418" ;
             } else {
-                d.innerHTML = "&nbsp;" ;
+                d.innerHTML = s[i-1] ;
             }
             r.appendChild(d);
         }
         let d = document.createElement("td");
         this.tbody.appendChild(r);
+        GV.add_history_row(s);
     }
 
     remove_row() {
         this.tbody.removeChild( this.tbody.lastChild ) ;
-    }
-
-    victory_row() {
     }
 
     header() {
@@ -267,13 +373,32 @@ class Table {
     }
     
 }
-var T = new Table();            
+var T = new Table();             
 
 function changeInput() {
     if ( H.change ) {
         T.start() ;
     }
 }
+
+class Overlay {
+	constructor () {
+		this.Garden( true ) ;
+	}
+	
+	Garden( onstate ) {
+		if ( onstate ) {
+			document.getElementById("svg").style.zIndex="1";
+			document.getElementById("Bgarden").style.backgroundColor = "white";
+			document.getElementById("Btable").style.backgroundColor = "grey";
+		} else {
+			document.getElementById("svg").style.zIndex="-1";
+			document.getElementById("Bgarden").style.backgroundColor = "grey";
+			document.getElementById("Btable").style.backgroundColor = "white";
+		}
+	}
+}
+var O = new Overlay();
 
 // Application starting point
 window.onload = () => {
